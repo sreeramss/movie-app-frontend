a30aedea1711a0414106c2de8a95a2f41e1b8c76
Sree Ram  S S <89720320+sreeramss@users.noreply.github.com>
2024-07-01 18:44:09 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -25,5 +25,5 @@ export const addBookmark = (bookmark) => API.post('/api/bookmarks', bookmark);
 export const deleteBookmark = (id) => API.delete(`/api/bookmarks/${id}`);
 export const deleteBookmarkByMovieId = (movieId) => API.delete(`/api/bookmarks/movie/${movieId}`);
 
-export const loginAPICall = (payload) => axios.post('http://localhost:5000/login/', payload);
-export const signupAPICall = (payload) => axios.post('http://localhost:5000/signup/', payload);
+export const loginAPICall = (payload) => axios.post('https://movie-app-backend-oqma.onrender.com', payload);
+export const signupAPICall = (payload) => axios.post('https://movie-app-backend-oqma.onrender.com', payload);
