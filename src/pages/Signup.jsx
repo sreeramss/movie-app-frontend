71a17791bb9f48e24c7333a048003d5c3e84b0bb
Sree Ram  S S <89720320+sreeramss@users.noreply.github.com>
2024-07-01 19:26:44 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -155,6 +155,7 @@ const Signup = () => {
           <button
             type="submit"
             className="w-full cursor-pointer rounded-lg bg-col-red px-4 py-2 text-white outline-none hover:bg-white hover:text-black"
+            
           >
             Create Account
           </button>
